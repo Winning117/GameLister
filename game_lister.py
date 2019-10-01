835e0612c94e4cce205a3de7dea525bbe3f2daa9
Joshua Correia <joshua.correia@Dexcom.com>
2019-10-01 08:00:11 -0700

    Added error message if no games were found

--- game_lister.py
+++ game_lister.py
@@ -2,6 +2,7 @@ def list_games(number_of_players):
     """
     Lists all of the games that you can play with the current number of players you have
     """
+    found_game = False
     with open("games.txt") as f:
         content = f.readlines()
         for line in content:
@@ -10,7 +11,11 @@ def list_games(number_of_players):
                 minimum_number_of_players = int(line.split(",")[1])
                 maximum_number_of_players = int(line.split(",")[2])
                 if number_of_players >= minimum_number_of_players and number_of_players <= maximum_number_of_players:
+                    found_game = True
                     print(f"{game_name} - {minimum_number_of_players} to {maximum_number_of_players} players")
+
+        if not found_game:
+            print(f"ERROR: Unable to find a game that's suitable for {number_of_players} players!")
 
 
 def main():
